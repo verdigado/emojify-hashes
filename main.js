a9b1aa60470008cf79c6ca18199c01ca13a78410
Henrik Hüttemann <henrik.huettemann@verdigado.com>
2021-10-29 21:21:20 +0200

    INIT

---
+++ main.js
@@ -0,0 +1,28 @@
+import Emojifier from './Emojifier.js'
+import Sha256 from './Sha256.js'
+import readline from 'readline'
+
+console.log('Type something in to generate it\'s sha256 hash and an emoji representation of that.')
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'toSha256ToEmoji> '
+})
+
+rl.prompt()
+
+rl.on('line', (line) => {
+  const hash = Sha256.hash(line.trim())
+  const emojis = Emojifier.emojify(hash)
+
+  console.log(
+    hash,
+    emojis
+  )
+
+  rl.prompt()
+}).on('close', () => {
+  console.log('Have a great day!')
+  process.exit(0)
+})
