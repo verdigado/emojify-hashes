d422e5b5622da2d101ba078ad8fb1ecdfc9d889e
Henrik Hüttemann <henrik.huettemann@verdigado.com>
2021-11-05 20:57:25 +0100

    Add more emojify tests

--- test/test.js
+++ test/test.js
@@ -1,14 +1,29 @@
 import assert from 'assert'
 import emojify from '../emojify.js'
-import { it } from 'mocha'
 
 describe('emojify', function () {
-  describe('README:', function () {
-    it('works as stated in the example', function () {
-      assert.deepEqual(
-        emojify('f3d18279b58b78a7e41fc6ec93f00f4dde045463ec21a527714e65c30553a52d'),
-        [ '🍸', '🥧', '🐏', '🐗' ]
-      )
-    })
+  it('works as stated in the README\'s example', function () {
+    assert.deepEqual(
+      emojify('f3d18279b58b78a7e41fc6ec93f00f4dde045463ec21a527714e65c30553a52d'),
+      [ '🍸', '🥧', '🐏', '🐗' ]
+    )
+  })
+
+  it('works with a generic aplhabet', function () {
+    assert.deepEqual(
+      emojify('01af', [ 0, 1, 2, 3 ]),
+      [ 0, 1, 2, 3 ]
+    )
+  })
+
+  it('throws on other than hex input', function () {
+    const input = '01afz'
+    assert.throws(
+      () => { emojify(input) },
+      {
+        name: 'RangeError',
+        message: `Input "${input}" is not hexadecimal.`
+      }
+    )
   })
 })
